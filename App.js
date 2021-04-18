0457f53ab5b26010b5544353f32822e74dc31a5d
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-18 20:51:30 +0530

    feat: Added tab navigation

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Platform, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import reducers from './reducers'
 import middlewares from './middlewares'
 import Decks from './components/Decks';
+import AddDeck from './components/AddDeck'
+
+function DecksScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Decks />
+    </View>
+  );
+}
+
+function AddDeckScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <AddDeck />
+    </View>
+  );
+}
+
+const Tab =
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator()
 
 class App extends React.Component {
-
   render() {
     return (
       <Provider store={createStore(reducers, middlewares)}>
-        <View style={styles.container}>
+        <NavigationContainer>
           <StatusBar style="auto" />
-          <Decks />
-        </View>
+          <Tab.Navigator>
+            <Tab.Screen name="Decks" component={DecksScreen} />
+            <Tab.Screen name="Add New Deck" component={AddDeckScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </Provider>
     )
   } 
