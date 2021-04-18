1376ca021e7adc75c4fe14ef7c729e8c698c0c8a
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-18 18:45:56 +0530

    feat: Added Provider and store to App component

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers'
+import middlewares from './middlewares'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <Provider store={createStore(reducers, middlewares)}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </Provider>
+    )
+  } 
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
